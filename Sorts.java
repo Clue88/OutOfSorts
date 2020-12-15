3c78e33f3dd7890f2df2371be0a12e692df287e4
Christopher Liu <christopherliu@sorcero.com>
2020-12-15 14:28:00 -0500

    Add tester - need to expand later

--- Sorts.java
+++ Sorts.java
@@ -1,20 +1,16 @@
-import java.util.Arrays;
 public class Sorts {
     public static void bubbleSort(int[] data) {
-        for (int i = data.length; i >= 0; i++) {
-            System.out.println(Arrays.toString(data));
+        for (int i = data.length; i >= 0; i--) {
             boolean swapped = false;
-            for (int j = 0; j < i - 1; j++) {
-                if (data[j] > data[j+1]) {
+            for (int j = 1; j < i; j++) {
+                if (data[j] < data[j-1]) {
+                    int old = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = old;
                     swapped = true;
-                    int old_a = data[j];
-                    data[j] = data[j+1];
-                    data[j+1] = old_a;
                 }
             }
-            if (!swapped) {
-                return;
-            }
+            if (!swapped) return;
         }
     }
 }
---
+++ Tester.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Tester {
+    public static void main(String[] args) {
+        int[] a = {5, 1, 12, -5, 16};
+        Sorts.bubbleSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
